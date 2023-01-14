c608345ce4579f31cfc35b7faca3d4a2023ca860
Rafael Pilartes <rafaelpilartes.rlps@gmail.com>
2023-01-14 18:12:39 +0100

    remove import

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Layout } from '../Layout'
-import CountdownTimer from '../pages/Countdown'
 import Home from '../pages/Home'
 
 export function Router() {
